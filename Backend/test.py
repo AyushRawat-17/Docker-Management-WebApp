d8665b119a2de2cfebde643b6bfd047fa662b8d4
AyushRawat-17 <ayushrawatkv@gmail.com>
2022-01-17 15:24:21 +0530

    Removed unnessary test file

--- Backend/test.py
+++
@@ -1,7 +0,0 @@
-import Backend.dockerHandling as dockerHandling
-
-object = dockerHandling.DockerHandling('tcp://192.168.56.106:2700')
-
-# op = object.dockerImagesBuild("C:\\Users\\ayush\\Desktop\\test\\","myimg")
-op = object.dockerImageslist()
-print(op)

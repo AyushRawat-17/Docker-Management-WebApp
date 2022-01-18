bab0f3cc9835f7fed0c5f365bdc1fa7d5ca8c81c
AyushRawat-17 <ayushrawatkv@gmail.com>
2022-01-18 17:58:47 +0530

    Some minor changes in frontend

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import Header from './Header'
+import Card from './Card'
 
 function Home() {
     return (
         <div className='Home'>
             <Header/>
+            <Card/>
         </div>
     )
 }
